44e03ad174ae0c80064298d8216df71c59508a3d
norven63 <norven63@163.com>
2013-07-15 15:45:29 +0800

    2

---
+++ HelloWorld/src/com/myAndroid/helloworld/MyApplication.java
@@ -0,0 +1,14 @@
+package com.myAndroid.helloworld;
+
+import java.io.File;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		File file = getFilesDir();//路径为/data/data/com.myAndroid.helloworld/files
+		System.out.println();
+	}
+}
