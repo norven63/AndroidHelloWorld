6fbb5aab11df2b07e16593104a47d8f6496ad95a
norven63 <norven63@163.com>
2013-07-18 09:34:26 +0800

    send message

---
+++ HelloWorld/src/com/myAndroid/helloworld/service/SendMessageService.java
@@ -0,0 +1,22 @@
+package com.myAndroid.helloworld.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.SmsManager;
+
+public class SendMessageService extends Service {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		SmsManager smg = SmsManager.getDefault();
+		// smg.sendTextMessage("18500195263", null, "HelloWorld", null, null);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+}
