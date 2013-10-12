b83a4b0b69b8eeaeb1c4ca8fb0cc14ed7f8dbd49
norven63 <norven63@163.com>
2013-10-12 09:43:10 +0800

    test

--- HelloWorld/src/main/java/com/myAndroid/helloworld/activity/SendOrderReceiverActivity.java
+++ HelloWorld/src/main/java/com/myAndroid/helloworld/activity/SendOrderReceiverActivity.java
@@ -1,19 +1,10 @@
 package com.myAndroid.helloworld.activity;
 
 import com.myAndroid.helloworld.R;
-
-import android.content.Context;
-
-import android.os.Environment;
-
 import android.util.Log;
-
 import android.content.Intent;
-
 import android.view.View;
-
 import android.os.Bundle;
-
 import android.app.Activity;
 
 public class SendOrderReceiverActivity extends Activity {
