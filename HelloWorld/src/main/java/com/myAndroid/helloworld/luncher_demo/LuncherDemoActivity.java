81f7d9ac2163b2760ae51859de88f962ebeeb917
norven63 <norven63@163.com>
2013-11-13 11:31:21 +0800

    Lunceher Demo V1.3

--- HelloWorld/src/main/java/com/myAndroid/helloworld/luncher_demo/LuncherDemoActivity.java
+++ HelloWorld/src/main/java/com/myAndroid/helloworld/luncher_demo/LuncherDemoActivity.java
@@ -5,6 +5,8 @@ import com.myAndroid.helloworld.R;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.os.Bundle;
@@ -27,8 +29,47 @@ public class LuncherDemoActivity extends RoboActivity implements OnLongClickList
   private Handler handler = new Handler() {
     @Override
     public void handleMessage(Message msg) {
-      View view = (View) msg.obj;
-      view.setAlpha((float) 0.3);
+      ViewGroup parentView = (ViewGroup) msg.obj;
+      View view = parentView.getChildAt(msg.arg1);
+      final View dragView = parentView.getChildAt(msg.arg2);
+
+      // 监听View的坐标
+      float x = view.getX();
+      float y = view.getY();
+
+      // 拖动View的原位置坐标
+      float x_ = dragView.getX();
+      float y_ = dragView.getY();
+
+      ((ViewGroup) dragView.getParent()).removeView(dragView);
+      parentView.addView(dragView, msg.arg1);
+
+      // 在ViewGroup对拖动View的新增&删除操作完毕后,先将拖动View重新布局至原位置,然后再开启从原位置移动至新位置的动画
+      dragView.setX(x_);
+      dragView.setY(y_);
+      dragView.animate().x(x).y(y).setDuration(620).setListener(new AnimatorListener() {
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+          // 恢复标记位,允许ViewGroup作出将本尊移回原位的响应操作
+          isItemOnDragListener = true;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationStart(Animator animation) {
+          // 设置标记位,不让ViewGroup作出将本尊移回原位的响应操作
+          isItemOnDragListener = false;
+        }
+      });
+
     }
   };
 
@@ -50,69 +91,58 @@ public class LuncherDemoActivity extends RoboActivity implements OnLongClickList
     viewDragListener = new OnDragListener() {
       @Override
       public boolean onDrag(final View view, DragEvent event) {
-        Timer timer = (Timer) view.getTag(R.id.timer);
-
         final View dragView = (View) event.getLocalState();
 
-        final GridLayout newViewGroup = (GridLayout) view.getParent();
+        // 如果拖动事件遮蔽自己则直接return
+        if (view.equals(dragView)) {
+          return false;
+        }
 
-        final float x = view.getX();
-        final float y = view.getY();
+        Timer timer = (Timer) view.getTag(R.id.timer);
+
+        final GridLayout parentView = (GridLayout) view.getParent();
+        final int index = parentView.indexOfChild(view);
 
         switch (event.getAction()) {
           case DragEvent.ACTION_DRAG_ENTERED:
-            // 开启计时器,只有当拖动图标在目标图标处停留1秒后才会有效果
+            // 开启计时器,在只有当拖动View在监听View处停留0.7秒后才会有效果
             timer.schedule(new TimerTask() {
               @Override
               public void run() {
                 handler.post(new Runnable() {
                   @Override
                   public void run() {
+                    // 发送Message以触发Handle处理,其中包含了ViewGroup对象,监听View和拖动View的索引
                     Message message = new Message();
-                    message.obj = view;
+                    message.obj = parentView;
+                    message.arg1 = index;
+                    message.arg2 = parentView.indexOfChild(dragView);
+
                     handler.sendMessage(message);
                   }
                 });
               }
-            }, 1000);
+            }, 700);
 
             break;
 
           case DragEvent.ACTION_DRAG_EXITED:
+            // 取消定时器,并重置
             timer.cancel();
             view.setTag(R.id.timer, new Timer());
-            view.setAlpha((float) 1.0);
 
             break;
 
           case DragEvent.ACTION_DROP:
-            if (!view.equals(dragView)) {
-              if (view.getAlpha() == (float) 0.3) {
-                final int index = newViewGroup.indexOfChild(view);
+            if (view.getAlpha() == (float) 0.3) {
 
-                // 保持本尊的原位置坐标
-                int l = dragView.getLeft();
-                int t = dragView.getTop();
-                int r = dragView.getRight();
-                int b = dragView.getBottom();
-
-                ((ViewGroup) dragView.getParent()).removeView(dragView);
-                newViewGroup.addView(dragView, index);
-
-                // 在ViewGroup操作完毕后,先将本尊重新布局至原位置,然后再开启从原位置移动至新位置的动画
-                dragView.layout(l, t, r, b);
-                dragView.animate().x(x).y(y);
-
-                // 设置标记位,不让ViewGroup作出将本尊移回原位的响应操作
-                isItemOnDragListener = false;
-              }
             }
 
             dragView.setAlpha((float) 1.0);
 
+            // 取消定时器,并重置
             timer.cancel();
             view.setTag(R.id.timer, new Timer());
-            view.setAlpha((float) 1.0);
 
             // 此处返回false是为了让ViewGroup能够捕获到DragEvent.ACTION_DROP事件!!!
             return false;
@@ -132,7 +162,7 @@ public class LuncherDemoActivity extends RoboActivity implements OnLongClickList
       view.setOnDragListener(viewDragListener);
       view.setOnLongClickListener(this);
 
-      // 注意此处key的定义,是在res/values/string.xml文件中定义的一个<item>表情
+      // 注意此处key的定义,是在res/values/string.xml文件中定义的一个<item>标签
       view.setTag(R.id.timer, new Timer());
     }
 
@@ -144,19 +174,26 @@ public class LuncherDemoActivity extends RoboActivity implements OnLongClickList
 
         switch (event.getAction()) {
           case DragEvent.ACTION_DROP:
-            if (isItemOnDragListener) {
-              dragView.setAlpha((float) 1.0);
+            dragView.setAlpha((float) 1.0);
 
+            float e_x = event.getX() - dragView.getWidth() / 2;
+            float e_y = event.getY() - dragView.getHeight() / 2;
+
+            if (isItemOnDragListener) {
               // 从拖动点移回至原位的动画
               float x = dragView.getX();
               float y = dragView.getY();
 
-              dragView.setX(event.getX() - dragView.getWidth() / 2);
-              dragView.setY(event.getY() - dragView.getHeight() / 2);
+              dragView.setX(e_x);
+              dragView.setY(e_y);
 
               dragView.animate().x(x).y(y);
-
+            } else {
+              // 两个View的移位动画未结束时就拦截住DROP事件处理方案
+              // TODO
             }
+
+            break;
 
           default:
             break;
