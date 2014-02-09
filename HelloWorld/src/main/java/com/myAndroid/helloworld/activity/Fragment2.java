5e0034c4726b713d23b6f676e52e2e6c11cd6870
norven63 <norven63@163.com>
2014-02-09 15:54:58 +0800

    s

--- HelloWorld/src/main/java/com/myAndroid/helloworld/activity/Fragment2.java
+++ HelloWorld/src/main/java/com/myAndroid/helloworld/activity/Fragment2.java
@@ -1,7 +1,5 @@
 package com.myAndroid.helloworld.activity;
 
-import com.myAndroid.helloworld.R;
-
 import android.annotation.SuppressLint;
 import android.app.ListFragment;
 import android.os.Bundle;
@@ -10,19 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import com.myAndroid.helloworld.R;
+
 @SuppressLint("NewApi")
 public class Fragment2 extends ListFragment {
-  public static final String[] DATALIST = { "囧", "囧", "囧", "囧" };
+	public static final String[] DATALIST = { "O", "O", "O", "O" };
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, DATALIST));
-  }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, DATALIST));
+	}
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    super.onCreateView(inflater, container, savedInstanceState);
-    return inflater.inflate(R.layout.fragment_list, container, false);
-  }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.fragment_list, container, false);
+	}
 }
