d37b78e5e6ac120e144f6e492d6850e4232a09dd
norven63 <norven63@163.com>
2014-02-21 17:28:12 +0800

    1

--- HelloWorld/src/main/java/com/myAndroid/helloworld/activity/SendOrderReceiverActivity.java
+++ HelloWorld/src/main/java/com/myAndroid/helloworld/activity/SendOrderReceiverActivity.java
@@ -1,30 +1,32 @@
 package com.myAndroid.helloworld.activity;
 
-import com.myAndroid.helloworld.R;
-import android.util.Log;
-import android.content.Intent;
-import android.view.View;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.myAndroid.helloworld.R;
 
 public class SendOrderReceiverActivity extends Activity {
-  public static final String RECEIVER_PERMISSION = "com.myAndroid.hellorld.orderReceiver.permission";
-  public static final String RECEIVER_ACTION = "com.myAndroid.hellorld.orderReceiver.action";
+	public static final String RECEIVER_PERMISSION = "com.myAndroid.hellorld.orderReceiver.permission";
+	public static final String RECEIVER_ACTION = "com.myAndroid.hellorld.orderReceiver.action";
 
-  public static final String DATA = "data";
+	public static final String DATA = "data";
 
-  public void sendOrderReceiver(View view) {
-    Intent intent = new Intent(RECEIVER_ACTION);
-    intent.putExtra(DATA, "Data from activity!");
-    sendOrderedBroadcast(intent, RECEIVER_PERMISSION);
+	// 按钮点击触发
+	public void sendOrderReceiver(View view) {
+		Intent intent = new Intent(RECEIVER_ACTION);
+		intent.putExtra(DATA, "Data from activity!");
+		sendOrderedBroadcast(intent, RECEIVER_PERMISSION);
 
-    Log.i("ORDER_RECEIVER", "Receiver has been sent!");
-  }
+		Log.i("ORDER_RECEIVER", "Receiver has been sent!");
+	}
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.order_receiver);
-  }
+		setContentView(R.layout.order_receiver);
+	}
 }
