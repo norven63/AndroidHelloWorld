3410e84e0275edbd46ba9a8d91344a5b70f23d40
norven63 <norven63@163.com>
2014-04-03 17:50:26 +0800

    加入一个开源的缩放图片控件

---
+++ HelloWorld/src/main/java/com/myAndroid/helloworld/activity/GestureImageViewActivity.java
@@ -0,0 +1,15 @@
+package com.myAndroid.helloworld.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.myAndroid.helloworld.R;
+
+public class GestureImageViewActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_gesture_imageview);
+	}
+}
